143252d35720a020a4e5317372fef238ef707048
moonhee0507 <sjmh0507@naver.com>
2022-06-27 15:42:37 +0900

    ✨Post 카테고리 컴포넌트 추가

---
+++ src/components/postview/PostCategories.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import data from "../../data.json";
+
+const StyledDl = styled.dl`
+  margin: 2rem 0 1rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  color: var(--main-color);
+
+  dd {
+    margin-right: 0.4rem;
+    padding: 0.2rem 1.2rem;
+    border-radius: calc(var(--border-radius) * 2);
+    background: var(--gray-background);
+    font-size: 1.2rem;
+    color: var(--main-color);
+  }
+`;
+
+function PostCategories() {
+  const { postId } = useParams();
+  
+  const post = data.posts.find((item) => {
+    return item.id == postId;
+  });
+
+  return (
+    <StyledDl>
+      <dt class="sr-only">Category</dt>
+      <dd>{post.category[0]}</dd>
+      <dd>{post.category[1]}</dd>
+    </StyledDl>
+  )
+}
+
+export default PostCategories;
