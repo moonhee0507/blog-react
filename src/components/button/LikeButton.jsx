e22a6c0f4e39064e946b10f3bcae7a5d813b341d
moonhee0507 <sjmh0507@naver.com>
2022-06-29 02:13:50 +0900

    💄 포스트뷰페이지 좋아요 버튼 컴포넌트 구현

---
+++ src/components/button/LikeButton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import IconLike from "../../assets/icon-like.svg";
+import IconLikeWhite from "../../assets/icon-like-white.svg";
+
+const StyledLikeButton = styled.button`
+    font-size: initial;
+    flex-shrink: 0;
+    width: 12rem;
+    height: 4rem;
+    padding-left: 3rem;
+    border: 1px solid rgba(var(--red-rgb), 0.5);
+    border-radius: var(--border-radius);
+    background: var(--white-color) url(${IconLike}) no-repeat 2.5rem 50% / 2.4rem;
+    transition: all 0.3s;
+
+    &:hover, 
+    &:active {
+        background: var(--red-color) url(${IconLikeWhite}) no-repeat 2.5rem 50% / 2.4rem;
+        color: var(--white-color);
+    }
+
+    // TODO: focus되도 outline 안그려지는 현상(1s뒤 실행하도록 해보기)
+    &:focus {
+        outline-color: rgba(var(--red-rgb), 0.2);
+        box-shadow: none;
+    }
+`;
+
+function LikeButton() {
+    return (
+        <StyledLikeButton className="button-like">Like</StyledLikeButton>
+    )
+}
+
+export default LikeButton;
