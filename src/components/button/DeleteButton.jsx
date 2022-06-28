edec8e25a090a577ad0664ed6c2e1438ff6f8cd8
moonhee0507 <sjmh0507@naver.com>
2022-06-29 02:13:19 +0900

    💄포스트뷰페이지 삭제버튼 컴포넌트 구현

---
+++ src/components/button/DeleteButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import IconDelete from "../../assets/icon-delete.svg";
+import IconDeleteWhite from "../../assets/icon-delete-white.svg";
+
+const StyledDeleteButton = styled.button`
+    background: var(--gray-background) url(${IconDelete}) no-repeat 50% 50% / 1.6rem;
+    right: 13rem;
+
+    width: 4rem;
+    height: 4rem;
+    border: 0;
+    border-radius: var(--border-radius);
+    transition: all 0.3s;
+    
+    :hover {
+        transition: all 0.3s;
+        background: var(--red-color) url(${IconDeleteWhite}) no-repeat 50% 50% / 1.6rem;
+    }
+`;
+
+function DeleteButton(props) {
+    return (
+        <StyledDeleteButton >
+            <span class="sr-only">delete</span>
+        </StyledDeleteButton>
+    );
+}
+
+export default DeleteButton;
