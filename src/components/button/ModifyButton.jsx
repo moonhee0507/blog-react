a7bb46d52b87257338db16a5360dafb0ec76e170
moonhee0507 <sjmh0507@naver.com>
2022-06-29 02:14:24 +0900

    💄 포스트뷰페이지 수정버튼 컴포넌트 구현

---
+++ src/components/button/ModifyButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import IconModify from "../../assets/icon-modify.svg";
+import IconModifyWhite from "../../assets/icon-modify-white.svg";
+
+const StyledModifyButton = styled(Link)`
+    background: var(--gray-background) url(${IconModify}) no-repeat 50% 50% / 1.6rem;
+    right: 13rem;
+    width: 4rem;
+    height: 4rem;
+    border: 0;
+    border-radius: var(--border-radius);
+    
+    :hover {
+        transition: all 0.3s;
+        background: var(--main-color) url(${IconModifyWhite}) no-repeat 50% 50% / 1.6rem;
+    }
+`;
+
+function ModifyButton(props) {
+    return (
+        <StyledModifyButton to="/">
+            <span class="sr-only">modify</span>
+        </StyledModifyButton>
+    );
+}
+
+export default ModifyButton;
