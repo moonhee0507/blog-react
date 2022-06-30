4557a226c26579c94e859de30483364b62de0668
moonhee0507 <sjmh0507@naver.com>
2022-06-30 17:18:51 +0900

    :sparkles: 좋아요 버튼 카운트(0, 1) 및 스타일 적용

--- src/components/button/LikeButton.jsx
+++ src/components/button/LikeButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import IconLike from "../../assets/icon-like.svg";
 import IconLikeWhite from "../../assets/icon-like-white.svg";
@@ -11,25 +11,42 @@ const StyledLikeButton = styled.button`
     padding-left: 3rem;
     border: 1px solid rgba(var(--red-rgb), 0.5);
     border-radius: var(--border-radius);
-    background: var(--white-color) url(${IconLike}) no-repeat 2.5rem 50% / 2.4rem;
     transition: all 0.3s;
 
-    &:hover, 
-    &:active {
-        background: var(--red-color) url(${IconLikeWhite}) no-repeat 2.5rem 50% / 2.4rem;
-        color: var(--white-color);
-    }
-
-    &:focus {
+    &:hover {
         outline: rgba(var(--red-rgb), 0.2) solid;
         box-shadow: none;
     }
 `;
 
-function LikeButton() {
+function LikeButton(props) {
+    const [likesCount, setLikesCount] = useState(0);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        if (isClicked) {
+            setLikesCount(likesCount - 1);
+        } else {
+            setLikesCount(likesCount + 1);
+        }
+        setIsClicked(!isClicked);
+    };
+
+    console.log('likesCount', likesCount);
+
     return (
-        <StyledLikeButton className="button-like">Like</StyledLikeButton>
-    )
+        <StyledLikeButton 
+            onClick={ handleClick }
+            style={{ background: isClicked ? 
+                `var(--red-color) url(${IconLikeWhite}) no-repeat 2.5rem 50% / 2.4rem` 
+                : 
+                `var(--white-color) url(${IconLike}) no-repeat 2.5rem 50% / 2.4rem`, 
+                     color: isClicked ? 'white' : 'black'
+            }}
+        >
+            Like
+        </StyledLikeButton>
+    );
 }
 
 export default LikeButton;
