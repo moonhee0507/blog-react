45862d617df341daa5c26ecf271ba280ede29c94
moonhee0507 <sjmh0507@naver.com>
2022-07-01 22:06:20 +0900

    :sparkles: PostContentItem 데이터 연결 및 스타일 적용

---
+++ src/components/postview/PostContentItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import data from "../../data.json";
+
+
+const StyledP = styled.p`
+    font-size: 1.6rem;
+    line-height: 1.6;
+    text-align: justify;
+    letter-spacing: -0.01em;
+`;
+
+const StyledPostImage = styled.img`
+    width: 100%;
+    margin: 3.2rem 0;
+    vertical-align: bottom;
+`;
+
+function PostContentItem(props) {
+    const { postId } = useParams();
+    const post = data.posts.find((item) => {
+        return item.id == postId;
+    });
+
+    return (
+        <div>
+            {post.contents.map((item) => {
+                if (item.type === "p") {
+                    return <StyledP key={Math.random() * 100}>{item.text}</StyledP>;
+                }
+                if (item.type === "img") {
+                    return <StyledPostImage src={`../..${item.src}`}></StyledPostImage>;
+                }
+            })}
+        </div>
+    );
+}
+
+export default PostContentItem;
