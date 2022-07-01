035bb36fca095f5a71803fe4dc9c7c37bbf8a49b
moonhee0507 <sjmh0507@naver.com>
2022-07-01 22:10:11 +0900

    :speech_balloon: 주석 제거

--- src/pages/PostViewPage.jsx
+++ src/pages/PostViewPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-// import styled from "styled-components";
 import data from "../data.json";
 
 import PostBackground from "../components/postview/PostBackground";
@@ -8,28 +7,20 @@ import PostContent from "../components/postview/PostContent";
 
 
 function PostViewPage(props) {
-  const { postId } = useParams();
-  
-  
-  const post = data.posts.find((item) => {
-    return item.id == postId;
-  });
+    const { postId } = useParams();
+    
+    
+    const post = data.posts.find((item) => {
+        return item.id == postId;
+    });
 
-  console.log('post', post)
-  return (
-    <div>
-      <PostBackground />
-      <PostContent />
-      {/* {post.map((post, index, title) => {
-          return (
-            <h1
-              key={post.id}
-              post={post.title}
-            />
-          );
-        })} */}
-    </div>
-  );
+    console.log('post', post)
+    return (
+        <div>
+            <PostBackground />
+            <PostContent />
+        </div>
+    );
 }
 
 export default PostViewPage;
