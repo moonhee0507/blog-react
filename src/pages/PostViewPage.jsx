6077646443377bcfcb0e3829ebb3c88024b583e0
moonhee0507 <sjmh0507@naver.com>
2022-07-02 03:20:50 +0900

    :fire: 데이터 관련 불필요 코드 제거

--- src/pages/PostViewPage.jsx
+++ src/pages/PostViewPage.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import data from "../data.json";
-
 import PostBackground from "../components/postview/PostBackground";
 import PostContent from "../components/postview/PostContent";
 
 
 function PostViewPage(props) {
-    const { postId } = useParams();
-    
-    
-    const post = data.posts.find((item) => {
-        return item.id == postId;
-    });
-
-    console.log('post', post)
     return (
         <div>
             <PostBackground />
